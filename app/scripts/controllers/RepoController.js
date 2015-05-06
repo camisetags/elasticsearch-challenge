0b197e693c8ef2a4ac3277d211a8751711b6ec38
gabrielSeixas <gabriel.seixas@paramaker.com.br>
2015-05-06 12:42:37 -0300

    resource

--- app/scripts/controllers/RepoController.js
+++ app/scripts/controllers/RepoController.js
@@ -9,13 +9,24 @@
  */
 RepoApp.controller('IndexRepoController', function ($scope, Repo) {
     $scope.repos = false;
-    Repo.list(function (response) {
-        $scope.repos = response.items;  
+//    ('/:repo/:org/:reponame/:commit', { repo: '@repo', org: '@org', repomane: '@reponame', commit: '@commit' });
+//    /search/repositories?q=yeoman&sort=stars&order=desc
+    
+    Repo.get({
+        type: 'search',
+        org: 'repositories',
+        q: 'yeoman',
+        sort: 'stars',
+        order: 'desc'
+    }, function (response) {
+        $scope.repos = response.items;
     });
 });
 
 RepoApp.controller('ShowRepoController', function ($scope, $routeParams, Repo, $http, $location) {
     $scope.page = 0;
+    var per_page = 20;
+    $scope.finish = false;
     
     $scope.activate = function (item) {
         $scope.selected = item;
@@ -27,25 +38,42 @@ RepoApp.controller('ShowRepoController', function ($scope, $routeParams, Repo, $
     
     var params = '/commits?page=0&per_page=5';
     
-    $http.get('https://api.github.com/repos/yeoman/'+ $routeParams.repo)
-        .success(function (data, status, headers, config) {
-            $scope.stars = data.stargazers_count;
-            $scope.forks = data.forks_count;
-            $scope.contribs = data.watchers_count;
-        });
+    Repo.get({
+        type: 'repos',
+        org: 'yeoman',
+        reponame: $routeParams.repo
+    },function (response) {
+        $scope.stars = response.stargazers_count;
+        $scope.forks = response.forks_count;
+        $scope.contribs = response.watchers_count;
+    });
     
-    $http.get('https://api.github.com/repos/yeoman/'+ $routeParams.repo + params)
-        .success(function (data, status, headers, config) {
-            $scope.commits = data;
-        });
-    
+    Repo.query({
+        type: 'repos',
+        org: 'yeoman',
+        reponame: $routeParams.repo,
+        commit: 'commits',
+        page: 0,
+        per_page: per_page
+    },function (response) {
+        $scope.commits = response;
+    });
     
 //    https://api.github.com/repos/yeoman/yeoman/commits?q=stargazers
     
     $scope.next_page = function () {
         $scope.page++;
+        Repo.query({
+            type: 'repos',
+            org: 'yeoman',
+            reponame: $routeParams.repo,
+            commit: 'commits',
+            page: $scope.page,
+            per_page: per_page
+        },function (response) {
+            for ( var i=0; i<per_page; i++ ){
+                $scope.commits.push(response[i]);;
+            }
+        });
     }
-    
-    
-    
 });
--- app/scripts/services/RepoService.js
+++ app/scripts/services/RepoService.js
@@ -1,31 +1,8 @@
-
-
-RepoApp.factory('Repo', function ($http, CONF) {
-    var url = CONF.apiEndpoint+'/search/repositories?q=yeoman&sort=stars&order=desc';
-    
-    function list (callback) {
-        $http.get(url)
-            .success(function (data, status, headers, config) {
-               callback(data);
-            });
-    }
-    
-    function show (repo, callback) {
-        $http.get('https://api.github.com/repos/yeoman/'+repo)
-            .success(function (data, status, headers, config) {
-                callback(data);
-            });
-    }
-    
-    
-    
-    return { 
-        list: list,
-        show: show,
-        
-    };
+RepoApp.factory('Repo', function ($resource, CONF) {
+    return $resource(CONF.apiEndpoint+'/:type/:org/:reponame/:commit', { 
+        type: '@type', 
+        org: '@org', 
+        repomane: '@reponame', 
+        commit: '@commit' 
+    });
 });
-
-//RepoApp.factory('RepoSearch', function ($resource, CONF) {
-//    return $resource(CONF.apiEndpoint+'/');
-//});
